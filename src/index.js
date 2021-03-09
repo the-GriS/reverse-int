6b341198c30280f2b2c4d0fc7946152b355126ed
the-GriS <grishhenjas@gmail.com>
2021-03-09 19:05:30 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    let ans = "";
+    while (n > 0) {
+        ans += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return ans;
+};
